708673a2ba87f6081876ea6348bda46d9718e59a
Henri-Louis Girard <hlgirard@users.noreply.github.com>
2018-11-14 15:10:11 -0500

    Update credentials.h

--- Arduino/credentials.h
+++ Arduino/credentials.h
@@ -1,6 +1,10 @@
+// Edit this file with your credentials
+
+// WiFi network credentials
 #define wifi_ssid "xxxxxxx"
 #define wifi_password "xxxxxxx"
 
+// Thinger.io credentials and bucket ID
 #define thinger_usrname "xxxxxxx"
 #define thinger_deviceID "xxxxxxxxx"
 #define thinger_deviceCred "xxxxxxxxx"
